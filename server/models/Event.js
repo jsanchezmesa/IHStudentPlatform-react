16c364955a33ab64189a0de8e65d72790c1d936b
Juan Sánchez Mesa <sanchezmesaj@gmail.com>
2018-11-13 11:55:31 +0100

    Event model created

---
+++ server/models/Event.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const eventSchema = new Schema({
+  title: String,
+  description: String,
+  date: Date,
+  location: {
+    type: { type: String },
+    coordinates: [Number]
+  },
+  author: { type: Schema.Types.ObjectId, ref: "User" },
+  imagePath: { type: String }
+}, {
+  timestamps: {
+    createdAt: "created_at",
+    updatedAt: "updated_at"
+  }
+});
+
+const Event = mongoose.model("Event", eventSchema);
+module.exports = Event;
