8970afe984d7f648d26ee7876633606d80ea547d
Juan Sánchez Mesa <sanchezmesaj@gmail.com>
2018-11-13 12:08:07 +0100

    isAdmin middleware created

--- server/middlewares/isActive.js
+++ server/middlewares/isActive.js
@@ -1,5 +1,5 @@
 module.exports = () => (req, res, next) => {
-  if(req.user.isActive) {
+  if(req.user && req.user.isActive) {
     next();
   } else {
     return res.status(403).json({message: "Active your account"});
