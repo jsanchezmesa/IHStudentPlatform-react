5e256887445dd6e1951849251c8f63a44d05c267
Juan Sánchez Mesa <sanchezmesaj@gmail.com>
2018-11-14 17:37:29 +0100

    Edit event created

--- server/middlewares/isAuthor.js
+++ server/middlewares/isAuthor.js
@@ -1,7 +1,16 @@
-module.exports = (author) => (req, res, next) => {
-  if(req.user && req.user._id == author) {
-    next();
+const Event = require("../models/Event");
+
+module.exports = () => (req, res, next) => {
+  if(req.user) {
+    Event.findById(req.params.id) 
+    .then(event => {
+      if(req.user._id.toString() == event.author.toString()) {
+        next();
+      } else {
+        return res.status(403).json({message: "Not author"});
+      }
+    })
   } else {
-    return res.status(403).json({message: "Not author"});
+    return res.status(403).json({message: "Not loggedin user"});
   }
 }
--- server/routes/event.js
+++ server/routes/event.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Event = require("../models/Event");
 const isActive = require("../middlewares/isActive");
+const isAuthor = require("../middlewares/isAuthor");
 const googleMapsClient = require("@google/maps").createClient({
   key: process.env.MAPSAPI,
   Promise: Promise
@@ -34,6 +35,44 @@ router.post("/new", isActive(), (req, res, next) => {
         .catch(err => res.status(500).json(err));
     })
     .catch(err => res.status(500).json(err));
+});
+
+router.post("/edit/:id", isAuthor(), (req, res, next) => {
+  const { title, description, address, date } = req.body;
+  const id = req.params.id;
+  const updates = {};
+
+  if(title) { updates['title'] = title; }
+  if(description) { updates['description'] = description; }
+  if(date) { updates['date'] = date; }
+
+  if(address) {
+
+    googleMapsClient
+      .geocode({ address })
+      .asPromise()
+      .then(data => {
+        let lat = data.json.results[0].geometry.viewport.northeast.lat;
+        let lng = data.json.results[0].geometry.viewport.northeast.lng;
+  
+        const location = {
+          type: "Point",
+          coordinates: [lat, lng]
+        };
+
+        updates['location'] = location;
+  
+        Event.findByIdAndUpdate(id, updates, {new: true})
+        .then(event => res.status(200).json(event))
+        .catch(err => res.status(500).json(err));
+      });
+  } else {
+    console.log(updates);
+    Event.findByIdAndUpdate(id, updates, {new: true})
+    .then(event => res.status(200).json(event))
+    .catch(err => res.status(500).json(err));
+  }
+
 });
 
 module.exports = router;
